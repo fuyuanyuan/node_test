1396c11e3103b0f378608bec30d05203b01e2798
fuyuanyuan <fuyuanyuan@smzdm.com>
2018-02-13 18:07:55 +0800

    生产环境构建

--- webpack_demo/server.js
+++ webpack_demo/server.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 
 const app = express();
-const config = require('./webpack.config.js');
+const config = require('./webpack.dev.js');
 const compiler = webpack(config);
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
@@ -20,6 +20,6 @@ app.use(devMiddleware);
 app.use(hotMiddleware);
 
 // Serve the files on port 3000.
-app.listen(3000, function () {
+app.listen(30001, function () {
     console.log('Example app listening on port 3000!\n');
 });
