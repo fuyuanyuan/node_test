d1252661c85666bbd91d75c6583622965f844db3
fuyuanyuan <fuyuanyuan@smzdm.com>
2018-02-13 15:15:29 +0800

    Output Management

--- webpack_demo/src/index.js
+++ webpack_demo/src/index.js
@@ -1,25 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import img from './img.jpg';
-import Data from './data.xml';
-import Dataj from './data.json';
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
     // Lodash，现在由此脚本导入
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    //css 添加类
-    element.classList.add('hello');
-
-    // 将图像添加到我们现有的 div。
-    var myimg = new Image();
-    myimg.src = img;
-    element.appendChild(myimg);
-
-    console.log(Data);
-    console.log(Dataj);
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
 
     return element;
 }
